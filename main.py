80bbdab7fd540c02bfc8a2cd4a432431a5372b22
Kieroxide <kieranbprivate@gmail.com>
2025-06-25 23:00:37 +0100

    Cleaned Main to use df always when operating but I keep a DF constant to maintain the original

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 
 def main():
     filename = "C:/Users/bailk/Desktop/Monzo Data Export - CSV (Friday, June 20th, 2025).csv"   
-    #formatting dataframe for graphable data
-    df = read_clean_csv(filename)
-    df_no_transfer = remove_category(df, category='Transfers')
-    df_no_savings = remove_category(df_no_transfer, category='Savings')
-    print(df_no_savings.to_string())
-    #Adds balance, income, spending column, DAILY
-    df_sum = df_no_savings.groupby('Date')['Amount'].sum().reset_index()
-    df_formatted = calc_running_Balance(df_sum)
-    ### NEED TO FIGURE OUT GOOD NAMING FOR ALL THESE DATAFRAMES
-    df_formatted['Income'] = df_formatted['Amount'].where(df_formatted['Amount'] > 0)
-    df_formatted['Spending'] = df_formatted['Amount'].where(df_formatted['Amount'] < 0)
-    print(df_formatted.to_string())
-    plot_continuous_graph(df_formatted, y_axes=['Balance', 'Amount'], colors=['blue', 'green'])
+
+    #Formatting dataframe for graphable data
+    DF = read_clean_csv(filename) #constant Dataframe
+
+    df = remove_category(DF, category='Transfers')
+    df = remove_category(df, category='Savings')
+
+    #Sums all transactions by date, calculates balance
+    df = df.groupby('Date')['Amount'].sum().reset_index()
+    df = calc_running_Balance(df)
+
+    #displays graph
+    plot_continuous_graph(df, y_axes=['Balance', 'Amount'], colors=['blue', 'green'])
     plt.show()
 
 def plot_continuous_graph(df, y_axes = [], colors = [], width = 12, height = 8):
@@ -23,18 +23,16 @@ def plot_continuous_graph(df, y_axes = [], colors = [], width = 12, height = 8):
     plt.figure(figsize=(width, height))
     for axis, color in zip(y_axes,colors):
         plot_line(df, y_axis=axis, color=color)
-
+    
+    plt.title("Continuous Balance Over Time")
+    plt.axhline(0, color='black') #X-axis Line
+    plt.xlabel("Date")
+    plt.grid(True)
+    plt.tight_layout()
     plt.legend()
 
 def plot_line(df, y_axis='Balance', color = 'blue'):
     plt.plot(df['Date'], df[y_axis], linewidth=1, color=color, label=y_axis)
-    plt.title("Continuous Balance Over Time")
-    plt.xlabel("Date")
-    plt.ylabel(y_axis)
-    #X-axis Line
-    plt.axhline(0, color='black')
-    plt.grid(True)
-    plt.tight_layout()
 
 def remove_category(df, category):
     #Removes rows with certain category
