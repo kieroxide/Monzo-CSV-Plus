0a07b611db4a563305b6a032592e8797819050eb
Kieroxide <kieranbprivate@gmail.com>
2025-06-26 13:49:17 +0100

    Filtered low percentages out to avoid overlapping labels

--- Category_Sum.py
+++ Category_Sum.py
@@ -7,33 +7,36 @@ def main():
     df = read_clean_csv(filename)
 
     #Formatting dataframe to series for plotting
-    categories_to_filter = ['Transfers', 'Savings']
+    categories_to_filter = ['Transfers', 'Savings', 'Bills']
     filtered_df = remove_categories(df, categories_to_filter)
     df_spending = filtered_df[filtered_df['Amount'] < 0]
     category_sum = df_spending.groupby('Category')['Amount'].sum()
     category_sum = category_sum.abs()
     category_sum = category_sum.sort_values()
-    
-    
+    total_spending = category_sum.sum()
+    category_percentages = (category_sum / total_spending) * 100
+    category_percentages = category_percentages[category_percentages > 2.5]
+    print(category_percentages)
+
     plt.figure(figsize=(10, 10))
-    category_sum.plot(
+    category_percentages.plot(
         kind='pie',
         autopct='%1.1f%%',
         startangle=90,
         counterclock=False,
         cmap='Set3',  
-        pctdistance=1.1,
+        pctdistance=1.15,
         wedgeprops={'edgecolor': 'white', 'linewidth': 1},
-        labels=None
+        labels=None,
     )
-
+    plt.ylabel("")
     plt.legend(
-        labels=category_sum.index,
+        labels=category_percentages.index,
         title="Categories",
         loc="center left",
         bbox_to_anchor=(1, 0.5)
     )
-    
+
     plt.show()
 
 
