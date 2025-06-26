830b67e6169f9cb9a4c316258addd7ffcd317b44
Kieroxide <kieranbprivate@gmail.com>
2025-06-26 14:18:37 +0100

    Separate code in Category_Sum into functions for readability

--- Category_Sum.py
+++ Category_Sum.py
@@ -7,37 +7,48 @@ def main():
     df = read_clean_csv(filename)
 
     #Formatting dataframe to series for plotting
+    category_percentages = format_for_pie(df)
+
+    #Plots pie
+    plt.figure(figsize=(10, 10))
+    plot_pie(category_percentages)
+    plt.show() #display pie
+
+def format_for_pie(df):
     categories_to_filter = ['Transfers', 'Savings', 'Bills']
     filtered_df = remove_categories(df, categories_to_filter)
+
     df_spending = filtered_df[filtered_df['Amount'] < 0]
+
+    #Sums, sorts and absolutes spending by category 
     category_sum = df_spending.groupby('Category')['Amount'].sum()
     category_sum = category_sum.abs()
     category_sum = category_sum.sort_values()
+
     total_spending = category_sum.sum()
+
     category_percentages = (category_sum / total_spending) * 100
     category_percentages = category_percentages[category_percentages > 2.5]
-    print(category_percentages)
+    return category_percentages
 
-    plt.figure(figsize=(10, 10))
-    category_percentages.plot(
+def plot_pie(df):
+    df.plot(
         kind='pie',
         autopct='%1.1f%%',
         startangle=90,
         counterclock=False,
         cmap='Set3',  
         pctdistance=1.15,
-        wedgeprops={'edgecolor': 'white', 'linewidth': 1},
+        wedgeprops={'edgecolor': 'grey', 'linewidth': 1},
         labels=None,
     )
+    plt.title("Spending Proportions")
     plt.ylabel("")
     plt.legend(
-        labels=category_percentages.index,
+        labels=df.index,
         title="Categories",
         loc="center left",
         bbox_to_anchor=(1, 0.5)
     )
-
-    plt.show()
-
 
 main()
